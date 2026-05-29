0c631ab69d9176d66ba4024ef079cee1a2d05a98
bernhardschmitter <bernhard.schmitter@t-online.de>
2026-05-29 19:28:52 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,39 +1,5 @@
-# Aktientool PWA Version 2
+# Aktientool PWA Version 3
 
-## Neu in dieser Version
+Neu: Indikatoren aus der Numbers-Struktur, Trendindikatoren separat, Depotanzeige mit Trend und Verkaufshinweis ab 1 Verkaufsindikator. Alle 144 Ticker aus der Numbers-Datei sind in data.js enthalten.
 
-- Watchlist als Startseite
-- Detailanalyse auf eigener Seite
-- Zurück-Button von der Detailanalyse
-- Depot auf eigener Seite
-- Zurück-Button vom Depot
-- Verkaufssignal direkt auf der Depotseite
-- Aktien hinzufügen am Ende der Startseite
-- Suchfeld, Signalfilter und Sortierung
-- Aktienliste aus der vorhandenen Watchlist als Startbestand
-- Kursdaten-Abruf über Stooq als erste Datenquelle
-- Fallback: Wenn keine Daten geladen werden können, bleiben vorhandene lokale Daten erhalten
-
-## GitHub Pages Update
-
-Alle Dateien direkt in das Repository hochladen und bestehende Dateien ersetzen:
-
-- index.html
-- style.css
-- data.js
-- app.js
-- manifest.json
-- service-worker.js
-- icon.svg
-- README.md
-
-Danach unter Actions/Pages kurz warten, bis GitHub die Seite neu veröffentlicht hat.
-
-## Hinweis zur Datenquelle
-
-Die App versucht Kursdaten von Stooq zu laden. Nicht jeder Ticker wird dort unter exakt demselben Symbol gefunden.
-Wenn ein Ticker nicht geladen wird, bleibt die Aktie trotzdem erhalten. Dann können wir das Symbol später gezielt anpassen.
-
-## Lokale Daten
-
-Die App speichert Daten im lokalen Browser-Speicher des Geräts. Bei einem Update der GitHub-Dateien bleiben deine lokalen Depotdaten normalerweise erhalten.
+Beim GitHub-Update alle Dateien ersetzen. Nach dem Hochladen ggf. Safari/Home-Screen-App neu laden oder neu hinzufügen, falls der alte Service Worker noch gecacht ist.
--- data.js
+++ data.js
@@ -1,474 +1,722 @@
 const INITIAL_STOCKS = [
   {
-    "symbol": "AAPL",
-    "name": "Apple"
-  },
-  {
-    "symbol": "MSFT",
-    "name": "Microsoft"
-  },
-  {
-    "symbol": "NVDA",
-    "name": "NVIDIA"
-  },
-  {
-    "symbol": "GOOG",
-    "name": "Alphabet"
-  },
-  {
-    "symbol": "AMZN",
-    "name": "Amazon"
-  },
-  {
-    "symbol": "META",
-    "name": "Meta Platforms"
-  },
-  {
-    "symbol": "TSLA",
-    "name": "Tesla"
-  },
-  {
-    "symbol": "ADBE",
-    "name": "Adobe"
-  },
-  {
-    "symbol": "ADSK",
-    "name": "Autodesk"
-  },
-  {
-    "symbol": "ABNB",
-    "name": "Airbnb"
-  },
-  {
-    "symbol": "AVGO",
-    "name": "Broadcom"
-  },
-  {
-    "symbol": "ASML",
-    "name": "ASML Holding"
-  },
-  {
-    "symbol": "ARM",
-    "name": "Arm Holdings"
-  },
-  {
-    "symbol": "AXON",
-    "name": "Axon Enterprise"
-  },
-  {
-    "symbol": "BKNG",
-    "name": "Booking Holdings"
-  },
-  {
-    "symbol": "CDNS",
-    "name": "Cadence Design Systems"
-  },
-  {
-    "symbol": "CDW",
-    "name": "CDW"
-  },
-  {
-    "symbol": "CEG",
-    "name": "Constellation Energy"
-  },
-  {
-    "symbol": "CHTR",
-    "name": "Charter Communications"
-  },
-  {
-    "symbol": "CMCSA",
-    "name": "Comcast"
-  },
-  {
-    "symbol": "COST",
-    "name": "Costco"
-  },
-  {
-    "symbol": "CPRT",
-    "name": "Copart"
-  },
-  {
-    "symbol": "CRWD",
-    "name": "CrowdStrike"
-  },
-  {
-    "symbol": "CSCO",
-    "name": "Cisco Systems"
-  },
-  {
-    "symbol": "CSGP",
-    "name": "CoStar Group"
-  },
-  {
-    "symbol": "CTAS",
-    "name": "Cintas"
-  },
-  {
-    "symbol": "CTSH",
-    "name": "Cognizant"
-  },
-  {
-    "symbol": "DASH",
-    "name": "DoorDash"
-  },
-  {
-    "symbol": "DDOG",
-    "name": "Datadog"
-  },
-  {
-    "symbol": "DXCM",
-    "name": "DexCom"
-  },
-  {
-    "symbol": "EA",
-    "name": "Electronic Arts"
-  },
-  {
-    "symbol": "FANG",
-    "name": "Diamondback Energy"
-  },
-  {
-    "symbol": "FAST",
-    "name": "Fastenal"
-  },
-  {
-    "symbol": "FTNT",
-    "name": "Fortinet"
-  },
-  {
-    "symbol": "GEHC",
-    "name": "GE HealthCare"
-  },
-  {
-    "symbol": "GILD",
-    "name": "Gilead Sciences"
-  },
-  {
-    "symbol": "IDXX",
-    "name": "IDEXX Laboratories"
-  },
-  {
-    "symbol": "INTC",
-    "name": "Intel"
-  },
-  {
-    "symbol": "INTU",
-    "name": "Intuit"
-  },
-  {
-    "symbol": "ISRG",
-    "name": "Intuitive Surgical"
-  },
-  {
-    "symbol": "KDP",
-    "name": "Keurig Dr Pepper"
-  },
-  {
-    "symbol": "KHC",
-    "name": "Kraft Heinz"
-  },
-  {
-    "symbol": "KLAC",
-    "name": "KLA"
-  },
-  {
-    "symbol": "LRCX",
-    "name": "Lam Research"
-  },
-  {
-    "symbol": "LULU",
-    "name": "Lululemon"
-  },
-  {
-    "symbol": "MAR",
-    "name": "Marriott"
-  },
-  {
-    "symbol": "MCHP",
-    "name": "Microchip Technology"
-  },
-  {
-    "symbol": "MDLZ",
-    "name": "Mondelez"
-  },
-  {
-    "symbol": "MELI",
-    "name": "MercadoLibre"
-  },
-  {
-    "symbol": "MNST",
-    "name": "Monster Beverage"
-  },
-  {
-    "symbol": "MRVL",
-    "name": "Marvell Technology"
-  },
-  {
-    "symbol": "MSTR",
-    "name": "Strategy"
-  },
-  {
-    "symbol": "NFLX",
-    "name": "Netflix"
-  },
-  {
-    "symbol": "NXPI",
-    "name": "NXP Semiconductors"
-  },
-  {
-    "symbol": "ODFL",
-    "name": "Old Dominion Freight Line"
-  },
-  {
-    "symbol": "ON",
-    "name": "ON Semiconductor"
-  },
-  {
-    "symbol": "ORLY",
-    "name": "O'Reilly Automotive"
-  },
-  {
-    "symbol": "PANW",
-    "name": "Palo Alto Networks"
-  },
-  {
-    "symbol": "PAYX",
-    "name": "Paychex"
-  },
-  {
-    "symbol": "PCAR",
-    "name": "PACCAR"
-  },
-  {
-    "symbol": "PDD",
-    "name": "PDD Holdings"
-  },
-  {
-    "symbol": "PEP",
-    "name": "PepsiCo"
-  },
-  {
-    "symbol": "PLTR",
-    "name": "Palantir"
-  },
-  {
-    "symbol": "PYPL",
-    "name": "PayPal"
-  },
-  {
-    "symbol": "QCOM",
-    "name": "QUALCOMM"
-  },
-  {
-    "symbol": "REGN",
-    "name": "Regeneron"
-  },
-  {
-    "symbol": "ROST",
-    "name": "Ross Stores"
-  },
-  {
-    "symbol": "SBUX",
-    "name": "Starbucks"
-  },
-  {
-    "symbol": "SNPS",
-    "name": "Synopsys"
-  },
-  {
-    "symbol": "TEAM",
-    "name": "Atlassian"
-  },
-  {
-    "symbol": "TMUS",
-    "name": "T-Mobile US"
-  },
-  {
-    "symbol": "TTWO",
-    "name": "Take-Two Interactive"
-  },
-  {
-    "symbol": "TXN",
-    "name": "Texas Instruments"
-  },
-  {
-    "symbol": "VRTX",
-    "name": "Vertex Pharmaceuticals"
-  },
-  {
-    "symbol": "VRSK",
-    "name": "Verisk Analytics"
-  },
-  {
-    "symbol": "WDAY",
-    "name": "Workday"
-  },
-  {
-    "symbol": "XEL",
-    "name": "Xcel Energy"
-  },
-  {
-    "symbol": "ZS",
-    "name": "Zscaler"
-  },
-  {
-    "symbol": "APP",
-    "name": "AppLovin"
-  },
-  {
-    "symbol": "AMGN",
-    "name": "Amgen"
-  },
-  {
-    "symbol": "BKR",
-    "name": "Baker Hughes"
-  },
-  {
-    "symbol": "GF",
-    "name": "GlobalFoundries"
-  },
-  {
-    "symbol": "HON",
-    "name": "Honeywell"
-  },
-  {
-    "symbol": "LIN",
-    "name": "Linde"
-  },
-  {
-    "symbol": "ADP",
-    "name": "Automatic Data Processing"
-  },
-  {
-    "symbol": "SAP.DE",
-    "name": "SAP"
-  },
-  {
-    "symbol": "SIE.DE",
-    "name": "Siemens"
+    "symbol": "ADS.DE",
+    "name": "ADS.DE",
+    "group": "DAX40"
   },
   {
     "symbol": "AIR.DE",
-    "name": "Airbus"
-  },
-  {
-    "symbol": "BAS.DE",
-    "name": "BASF"
-  },
-  {
-    "symbol": "BAYN.DE",
-    "name": "Bayer"
-  },
-  {
-    "symbol": "DTE.DE",
-    "name": "Deutsche Telekom"
-  },
-  {
-    "symbol": "EOAN.DE",
-    "name": "E.ON"
-  },
-  {
-    "symbol": "FME.DE",
-    "name": "Fresenius Medical Care"
-  },
-  {
-    "symbol": "FRE.DE",
-    "name": "Fresenius"
-  },
-  {
-    "symbol": "HEN3.DE",
-    "name": "Henkel Vz."
-  },
-  {
-    "symbol": "HNR1.DE",
-    "name": "Hannover Rück"
-  },
-  {
-    "symbol": "IFX.DE",
-    "name": "Infineon"
-  },
-  {
-    "symbol": "MTX.DE",
-    "name": "MTU Aero Engines"
-  },
-  {
-    "symbol": "MUV2.DE",
-    "name": "Münchener Rück"
-  },
-  {
-    "symbol": "PAH3.DE",
-    "name": "Porsche Holding"
-  },
-  {
-    "symbol": "P911.DE",
-    "name": "Porsche AG"
-  },
-  {
-    "symbol": "PUM.DE",
-    "name": "Puma"
-  },
-  {
-    "symbol": "RHM.DE",
-    "name": "Rheinmetall"
-  },
-  {
-    "symbol": "RWE.DE",
-    "name": "RWE"
-  },
-  {
-    "symbol": "SHL.DE",
-    "name": "Siemens Healthineers"
-  },
-  {
-    "symbol": "SRT3.DE",
-    "name": "Sartorius Vz."
-  },
-  {
-    "symbol": "SY1.DE",
-    "name": "Symrise"
-  },
-  {
-    "symbol": "VNA.DE",
-    "name": "Vonovia"
-  },
-  {
-    "symbol": "VOW3.DE",
-    "name": "Volkswagen Vz."
-  },
-  {
-    "symbol": "1COV.DE",
-    "name": "Covestro"
-  },
-  {
-    "symbol": "ADS.DE",
-    "name": "Adidas"
+    "name": "AIR.DE",
+    "group": "DAX40"
   },
   {
     "symbol": "ALV.DE",
-    "name": "Allianz"
+    "name": "ALV.DE",
+    "group": "DAX40"
+  },
+  {
+    "symbol": "BAS.DE",
+    "name": "BAS.DE",
+    "group": "DAX40"
+  },
+  {
+    "symbol": "BAYN.DE",
+    "name": "BAYN.DE",
+    "group": "DAX40"
+  },
+  {
+    "symbol": "BEI.DE",
+    "name": "BEI.DE",
+    "group": "DAX40"
+  },
+  {
+    "symbol": "BMW.DE",
+    "name": "BMW.DE",
+    "group": "DAX40"
+  },
+  {
+    "symbol": "BNR",
+    "name": "BNR",
+    "group": "DAX40"
   },
   {
     "symbol": "CBK.DE",
-    "name": "Commerzbank"
+    "name": "CBK.DE",
+    "group": "DAX40"
+  },
+  {
+    "symbol": "CON",
+    "name": "CON",
+    "group": "DAX40"
+  },
+  {
+    "symbol": "1COV.DE",
+    "name": "1COV.DE",
+    "group": "DAX40"
+  },
+  {
+    "symbol": "DTG",
+    "name": "DTG",
+    "group": "DAX40"
+  },
+  {
+    "symbol": "DBK.DE",
+    "name": "DBK.DE",
+    "group": "DAX40"
+  },
+  {
+    "symbol": "DB1.DE",
+    "name": "DB1.DE",
+    "group": "DAX40"
+  },
+  {
+    "symbol": "DHL.DE",
+    "name": "DHL.DE",
+    "group": "DAX40"
+  },
+  {
+    "symbol": "DTE.DE",
+    "name": "DTE.DE",
+    "group": "DAX40"
+  },
+  {
+    "symbol": "EOAN.DE",
+    "name": "EOAN.DE",
+    "group": "DAX40"
+  },
+  {
+    "symbol": "FME.DE",
+    "name": "FME.DE",
+    "group": "DAX40"
+  },
+  {
+    "symbol": "FRE.DE",
+    "name": "FRE.DE",
+    "group": "DAX40"
+  },
+  {
+    "symbol": "HNR1.DE",
+    "name": "HNR1.DE",
+    "group": "DAX40"
   },
   {
     "symbol": "HFG.DE",
-    "name": "HelloFresh"
+    "name": "HFG.DE",
+    "group": "DAX40"
   },
   {
-    "symbol": "NDAQ",
-    "name": "Nasdaq"
+    "symbol": "HEN3.DE",
+    "name": "HEN3.DE",
+    "group": "DAX40"
   },
   {
-    "symbol": "TQQQ",
-    "name": "ProShares UltraPro QQQ"
+    "symbol": "IFX.DE",
+    "name": "IFX.DE",
+    "group": "DAX40"
   },
   {
-    "symbol": "SXRV.DE",
-    "name": "iShares Nasdaq 100 ETF"
+    "symbol": "MBG.DE",
+    "name": "MBG.DE",
+    "group": "DAX40"
   },
   {
-    "symbol": "DAX.DE",
-    "name": "DAX ETF / Index-Platzhalter"
+    "symbol": "MRK.DE",
+    "name": "MRK.DE",
+    "group": "DAX40"
+  },
+  {
+    "symbol": "MTX.DE",
+    "name": "MTX.DE",
+    "group": "DAX40"
+  },
+  {
+    "symbol": "MUV2.DE",
+    "name": "MUV2.DE",
+    "group": "DAX40"
+  },
+  {
+    "symbol": "P911.DE",
+    "name": "P911.DE",
+    "group": "DAX40"
+  },
+  {
+    "symbol": "PAH3.DE",
+    "name": "PAH3.DE",
+    "group": "DAX40"
+  },
+  {
+    "symbol": "PUM.DE",
+    "name": "PUM.DE",
+    "group": "DAX40"
+  },
+  {
+    "symbol": "QIA.DE",
+    "name": "QIA.DE",
+    "group": "DAX40"
+  },
+  {
+    "symbol": "RHM.DE",
+    "name": "RHM.DE",
+    "group": "DAX40"
+  },
+  {
+    "symbol": "RWE.DE",
+    "name": "RWE.DE",
+    "group": "DAX40"
+  },
+  {
+    "symbol": "SAP.DE",
+    "name": "SAP.DE",
+    "group": "DAX40"
+  },
+  {
+    "symbol": "SRT3.DE",
+    "name": "SRT3.DE",
+    "group": "DAX40"
+  },
+  {
+    "symbol": "SIE.DE",
+    "name": "SIE.DE",
+    "group": "DAX40"
+  },
+  {
+    "symbol": "ENR.DE",
+    "name": "ENR.DE",
+    "group": "DAX40"
+  },
+  {
+    "symbol": "SHL.DE",
+    "name": "SHL.DE",
+    "group": "DAX40"
+  },
+  {
+    "symbol": "SY1.DE",
+    "name": "SY1.DE",
+    "group": "DAX40"
+  },
+  {
+    "symbol": "VOW3.DE",
+    "name": "VOW3.DE",
+    "group": "DAX40"
+  },
+  {
+    "symbol": "VNA.DE",
+    "name": "VNA.DE",
+    "group": "DAX40"
+  },
+  {
+    "symbol": "TESG.sg",
+    "name": "TESG.sg",
+    "group": "EFT"
+  },
+  {
+    "symbol": "ADBE",
+    "name": "ADBE",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "AMD",
+    "name": "AMD",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "ABNB",
+    "name": "ABNB",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "GOOGL",
+    "name": "GOOGL",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "GOOG",
+    "name": "GOOG",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "AMZN",
+    "name": "AMZN",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "AEP",
+    "name": "AEP",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "AMGN",
+    "name": "AMGN",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "ADI",
+    "name": "ADI",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "AAPL",
+    "name": "AAPL",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "AMAT",
+    "name": "AMAT",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "APP",
+    "name": "APP",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "ARM",
+    "name": "ARM",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "ASML",
+    "name": "ASML",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "AZN",
+    "name": "AZN",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "TEAM",
+    "name": "TEAM",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "ADSK",
+    "name": "ADSK",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "ADP",
+    "name": "ADP",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "AXON",
+    "name": "AXON",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "BKR",
+    "name": "BKR",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "BIB",
+    "name": "BIB",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "BKNG",
+    "name": "BKNG",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "AVGO",
+    "name": "AVGO",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "CDNS",
+    "name": "CDNS",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "CDW",
+    "name": "CDW",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "CHTR",
+    "name": "CHTR",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "CTAS",
+    "name": "CTAS",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "CSCO",
+    "name": "CSCO",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "CCEP",
+    "name": "CCEP",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "CTSH",
+    "name": "CTSH",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "CMCSA",
+    "name": "CMCSA",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "CEG",
+    "name": "CEG",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "CPRT",
+    "name": "CPRT",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "CSGP",
+    "name": "CSGP",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "COST",
+    "name": "COST",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "CRWD",
+    "name": "CRWD",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "CSX",
+    "name": "CSX",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "DDOG",
+    "name": "DDOG",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "DXCM",
+    "name": "DXCM",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "FANG",
+    "name": "FANG",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "DASH",
+    "name": "DASH",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "EA",
+    "name": "EA",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "EXC",
+    "name": "EXC",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "FAST",
+    "name": "FAST",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "FTNT",
+    "name": "FTNT",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "GEHC",
+    "name": "GEHC",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "GILD",
+    "name": "GILD",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "GFS",
+    "name": "GFS",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "HON",
+    "name": "HON",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "IDXX",
+    "name": "IDXX",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "INTC",
+    "name": "INTC",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "INTU",
+    "name": "INTU",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "ISRG",
+    "name": "ISRG",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "INTC",
+    "name": "INTC",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "KDP",
+    "name": "KDP",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "KLAC",
+    "name": "KLAC",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "KHC",
+    "name": "KHC",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "LRCX",
+    "name": "LRCX",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "LIN",
+    "name": "LIN",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "LULU",
+    "name": "LULU",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "MAR",
+    "name": "MAR",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "MRVL",
+    "name": "MRVL",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "MELI",
+    "name": "MELI",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "META",
+    "name": "META",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "MCHP",
+    "name": "MCHP",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "MU",
+    "name": "MU",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "MSFT",
+    "name": "MSFT",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "MSTR",
+    "name": "MSTR",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "MDLZ",
+    "name": "MDLZ",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "MNST",
+    "name": "MNST",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "NFLX",
+    "name": "NFLX",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "NVDA",
+    "name": "NVDA",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "NXPI",
+    "name": "NXPI",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "ORLY",
+    "name": "ORLY",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "ODFL",
+    "name": "ODFL",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "ON",
+    "name": "ON",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "PCAR",
+    "name": "PCAR",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "PLTR",
+    "name": "PLTR",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "PANW",
+    "name": "PANW",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "PAYX",
+    "name": "PAYX",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "PYPL",
+    "name": "PYPL",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "PDD",
+    "name": "PDD",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "PEP",
+    "name": "PEP",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "QCOM",
+    "name": "QCOM",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "REGN",
+    "name": "REGN",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "ROP",
+    "name": "ROP",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "ROST",
+    "name": "ROST",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "SHOP",
+    "name": "SHOP",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "SBUX",
+    "name": "SBUX",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "SNPS",
+    "name": "SNPS",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "TMUS",
+    "name": "TMUS",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "TTWO",
+    "name": "TTWO",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "TSLA",
+    "name": "TSLA",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "TXN",
+    "name": "TXN",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "TRI",
+    "name": "TRI",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "TTD",
+    "name": "TTD",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "VRSK",
+    "name": "VRSK",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "VRTX",
+    "name": "VRTX",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "WBD",
+    "name": "WBD",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "WDAY",
+    "name": "WDAY",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "XEL",
+    "name": "XEL",
+    "group": "Nasdaq"
+  },
+  {
+    "symbol": "ZS",
+    "name": "ZS",
+    "group": "Nasdaq"
   }
 ];
